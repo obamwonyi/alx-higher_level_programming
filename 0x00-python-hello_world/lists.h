9114bc4e58c5daee3bcbc77f2b544e0edf5a16fb
netweaver <developer.oduwa@gmail.com>
2023-11-01 23:46:34 +0100

    handled task 10

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,30 +1,3 @@
-#include "lists.h"
-
-/**
- * check_cycle - checks if a linked list contains a cycle
- * @list: linked list to check
- *
- * Return: 1 if the list has a cycle, 0 if it doesn't
- */
-int check_cycle(listint_t *list)
-{
-        listint_t *slow = list;
-        listint_t *fast = list;
-
-        if (!list)
-                return (0);
-
-        while (slow && fast && fast->next)
-        {
-                slow = slow->next;
-                fast = fast->next->next;
-                if (slow == fast)
-                        return (1);
-        }
-
-        return (0);
-}
-root@e024474929d2:~/alx-higher_level_programming/0x00-python-hello_world# cat lists.h
 #ifndef LISTS_H
 #define LISTS_H
 
