6919141b6d6c2cab55ddf1810cb15bb859c91106
netweaver <developer.oduwa@gmail.com>
2023-11-01 23:39:27 +0100

    handled task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * check_cycle - checks if a linked list contains a cycle
  * @list: linked list to check
@@ -8,19 +7,19 @@
  */
 int check_cycle(listint_t *list)
 {
-        listint_t *slow = list;
-        listint_t *fast = list;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
-        if (!list)
-                return (0);
+	if (!list)
+		return (0);
 
-        while (slow && fast && fast->next)
-        {
-                slow = slow->next;
-                fast = fast->next->next;
-                if (slow == fast)
-                        return (1);
-        }
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
 
-        return (0);
+	return (0);
 }
