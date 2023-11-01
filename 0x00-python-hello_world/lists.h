4c7279b744804a005f45d8762d1f10891a0bca48
netweaver <developer.oduwa@gmail.com>
2023-11-01 23:50:56 +0100

    handled task 10

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -2,7 +2,6 @@
 #define LISTS_H
 
 #include <stdlib.h>
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -13,8 +12,8 @@
  */
 typedef struct listint_s
 {
-        int n;
-        struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
