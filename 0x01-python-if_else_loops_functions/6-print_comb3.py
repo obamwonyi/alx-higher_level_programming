7686049f9eb057120d134237a2122079f57c05f8
netweaver <developer.oduwa@gmail.com>
2023-11-01 01:16:38 +0100

    handled task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,4 @@
 for i in range(10):
     for j in range(i, 10):
         if i < j:
-            print("{:d}{:d}".format(i, j),
-                    end="\n" if i == 8 and j == 9 ele ", ")
+            print("{:d}{:d}".format(i, j),end="\n" if i == 8 and j == 9 ele ", ")
