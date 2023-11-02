98a2e2399978a9fb5501aabb9feaeae6efbce20b
netweaver <developer.oduwa@gmail.com>
2023-11-02 01:02:12 +0100

    handled task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -7,7 +7,8 @@ def islower(c):
 
 
 def uppercase(str):
-    print("{:c}"
-          .format(ord(c) if not islower(c) else ord(c) - 32),
-          end="")
+    for c in str:
+        print("{:c}"
+              .format(ord(c) if not islower(c) else ord(c) - 32),
+              end="")
     print("")
