4a6d1d12995dfda3a573b3080127e70fbc0e5061
netweaver <developer.oduwa@gmail.com>
2023-11-06 23:50:17 +0100

    0x03. Python - Data Structures: Lists, Tuples:Task3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,4 @@ def print_reversed_list_integer(my_list=[]):
     if not my_list:
         return None
     for i in reversed(my_list):
-        print("{}".format(i))
+        print("{:d}".format(i))
