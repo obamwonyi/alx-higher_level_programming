40bca985fec5ca032d7cab97ca52b606bc6810ba
netweaver <developer.oduwa@gmail.com>
2023-11-06 22:19:02 +0100

    0x03. Python - Data Structures: Lists, Tuples:Task0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
 def print_list_integer(my_list=[]):
-    for i in range(0, len(my_list)):
-        if isinstance(my_list[i], int):
-            print("{}".format(my_list[i], end=""))
+    for i in my_list:
+        print("{}".format(i))
