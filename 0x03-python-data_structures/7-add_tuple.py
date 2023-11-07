d22a5cd72b68b9ed2eb288a4da115733b1256751
netweaver <developer.oduwa@gmail.com>
2023-11-07 01:22:44 +0100

    0x03. Python - Data Structures: Lists, Tuples:Task13

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a=(), tuple_b=()):
-    a1 = tuple_a[0] if len(tuple_a) > 0 else 0
-    a2 = tuple_a[1] if len(tuple_a) > 1 else 0
-    b1 = tuple_b[0] if len(tuple_b) > 0 else 0
-    b2 = tuple_b[1] if len(typle_b) > 1 else 0
-    return (a1 + b1, a2 + b2)
+add_tuple = __import__('7-add_tuple').add_tuple
+
+tuple_a = (1, 89)
+tuple_b = (88, 11)
+new_tuple = add_tuple(tuple_a, tuple_b)
+print(new_tuple)
+
+print(add_tuple(tuple_a, (1, )))
+print(add_tuple(tuple_a, ()))
+
