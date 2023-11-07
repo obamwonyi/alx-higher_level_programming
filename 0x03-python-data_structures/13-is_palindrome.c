95abcad258bbf140e4a4d332c5bca76509484444
netweaver <developer.oduwa@gmail.com>
2023-11-07 01:06:21 +0100

    0x03. Python - Data Structures: Lists, Tuples:Task13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,32 +1,54 @@
-#include "list.h"
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * is_palindrome - recursive palind or not
- * @head: head list
- * Return: 0 if it is not a palindrome
- */
+*add_nodeint - adds a new node at the beginning of a listint_t list
+*@head: head of listint_t
+*@n: int to add in listint_t list
+*Return: address of the new element, or NULL if it failed
+*/
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
+/**
+*is_palindrome - identify if a syngle linked list is palindrome
+*@head: head of listint_t
+*Return: 1 if it is palindrome else 0
+*/
 int is_palindrome(listint_t **head)
 {
-	if (head == NULL || *head == NULL)
-		return (1);
-	return (aux_palind(head, *head));
-}
+	listint_t *head2 = *head;
+	listint_t *aux = NULL, *aux2 = NULL;
 
-/**
- * aux_palind - funct to know if is palindrome
- * @head: head list
- * @end: end list
- * Return: 0 or 1
- */
-int aux_palind(listint_t **head, listint_t *end)
-{
-	if (end == NULL)
+	if (*head == NULL || head2->next == NULL)
 		return (1);
-
-	if (aux_palind(head, end->next) && (*head)->n == end->n)
+	while (head2 != NULL)
 	{
-		*head = (*head)->next;
-		return (1);
+		add_nodeint(&aux, head2->n);
+		head2 = head2->next;
 	}
-	return (0);
+	aux2 = aux;
+	while (*head != NULL)
+	{
+		if ((*head)->n != aux2->n)
+		{
+			free_listint(aux);
+			return (0);
+		}
+		*head = (*head)->next;
+		aux2 = aux2->next;
+	}
+	free_listint(aux);
+	return (1);
 }
+
