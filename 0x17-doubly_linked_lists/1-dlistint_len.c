a957fcc3121a65f61f23283b44a4dd428ea11809
netweaver <developer.oduwa@gmail.com>
2023-11-10 00:04:44 +0100

    0x17. C - Doubly linked lists, implemented task1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * size_t dlistint_len - returns the length of the dlist
+ * @h: address of the head node
+ * Return: size of list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
