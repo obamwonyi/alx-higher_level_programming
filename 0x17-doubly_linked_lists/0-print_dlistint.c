b47376cb7d2b501a09e8f08e326cf31fe4356574
netweaver <developer.oduwa@gmail.com>
2023-11-09 23:59:51 +0100

    0x17. C - Doubly linked lists, implemented task0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,1 +1,19 @@
-#include "list.h"
+#include "lists.h"
+
+/**
+ * print_dlistint - print out doubly linked list
+ * @h: address of the head node
+ * Return: size of the doubly linked list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
