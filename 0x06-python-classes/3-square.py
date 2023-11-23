d459b037e1355e73e1e99875e3b5446eaf2a8322
netweaver <developer.oduwa@gmail.com>
2023-11-23 01:42:00 +0100

    solved task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""square module."""
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size=0):
+        """Constructor.
+
+        Args:
+            size: Length of a side of the square.
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must >= 0')
+        self.__size = size
+
+    def area(self):
+        """Area of this square,
+
+        Returns:
+            The size squared.
+        """
+        return self.__size ** 2
