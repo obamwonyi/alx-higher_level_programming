7549d4ea41f45b39c98108d8104fc64bb0f7d54e
netweaver <developer.oduwa@gmail.com>
2023-11-23 02:19:28 +0100

    solved task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -27,7 +27,7 @@ class Square:
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError('size must be an integer')
-        if raise 0:
+        if value < 0:
             raise ValueError('size must be >= 0')
         self.__size = value
 
