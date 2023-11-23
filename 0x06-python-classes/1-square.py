5bce666cc6ddfa9b0ef35aa7812d919db02eed66
netweaver <developer.oduwa@gmail.com>
2023-11-23 01:23:14 +0100

    solved task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""square module."""
+
+
+class Square:
+    """Defines a square."""
+
+    def_init_(self, size):
+        """Constructor.
+        Args:
+            size: length of a side of the square.
+        """
+        self._size = size
