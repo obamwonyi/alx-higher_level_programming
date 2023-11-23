31b8569d8a800eee7d90fb71a16647d5782ce759
netweaver <developer.oduwa@gmail.com>
2023-11-23 01:25:56 +0100

    solved task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,7 +5,7 @@
 class Square:
     """Defines a square."""
 
-    def _init_(self, size):
+    def __init__(self, size):
         """Constructor.
         Args:
             size: length of a side of the square.
