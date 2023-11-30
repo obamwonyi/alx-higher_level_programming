31177272696ccb14553ecf64df49e1a261aab2da
netweaver <developer.oduwa@gmail.com>
2023-11-30 21:36:01 +0100

    fix for task 4

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
-"""Module for print_square method."""
+"""Defines a square-printing function."""
 
 
 def print_square(size):
-    """Method for printing a square with # characters.
+    """Print a square with the # character.
 
     Args:
-        size: The int size of the square's side.
-
-    Raises:
-        TypeError: If size is not an int.
-        ValueError: If size is < 0.
+        size (int): The height/width of the square.
+    Raise:
+        TypeError: If size is not an integer.
+        valueError: If size is < 0
     """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
-
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    print((("#" * size + "\n") * size), end="")
-
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/4-print_square.txt")
+    for i in range(size):
+        [print("#", end="") for j in range(size)]
+        print("")
