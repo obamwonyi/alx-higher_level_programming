6ef901b15503acdbaacc57762472cbf0e9df733d
netweaver <developer.oduwa@gmail.com>
2023-12-06 00:21:48 +0100

    re implemented task 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
-"""Defines a Pascal's Triangle function."""
-
+"""defining function"""
 
 def pascal_triangle(n):
-    """Represent Pascal's Triangle of size n.
-
-    Returns a list of lists of integers representing the triangle.
+    """Represent Pascal's Trangle of size n.
+        Returns a list of lists of integers representing the trangle.
     """
+
     if n <= 0:
         return []
 
-    triangles = [[1]]
-    while len(triangles) != n:
-        tri = triangles[-1]
+    trangles = [[1]]
+
+    while len(trangles) != n:
+        tri = trangles[-1]
         tmp = [1]
+
         for i in range(len(tri) - 1):
             tmp.append(tri[i] + tri[i + 1])
         tmp.append(1)
         triangles.append(tmp)
-    return triangles
+    return traingles
+
