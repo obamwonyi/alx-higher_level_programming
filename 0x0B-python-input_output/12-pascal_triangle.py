037c79fd6e99c2a2074d4db756f91406182049bc
netweaver <developer.oduwa@gmail.com>
2023-12-06 00:25:46 +0100

    re implemented task 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -10,14 +10,14 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    trangles = [[1]]
+    triangles = [[1]]
 
-    while len(trangles) != n:
-        tri = trangles[-1]
+    while len(triangles) != n:
+        tri = triangles[-1]
         tmp = [1]
 
         for i in range(len(tri) - 1):
             tmp.append(tri[i] + tri[i + 1])
         tmp.append(1)
         triangles.append(tmp)
-    return traingles
+    return triangles
