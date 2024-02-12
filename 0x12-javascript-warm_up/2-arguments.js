5f3a5719ae2ae6a7c786c974da9619c3e355081d
obamwonyi <developer.oduwa@gmail.com>
2024-02-12 18:10:02 +0100

    fixed the formating with semistandard

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-const argv = process.argv
+const argv = process.argv;
 
-if (argv.length > 2)
-{
-	console.log("Argument found")
-}
-else
-{
-	console.log("No argument")
+if (argv.length > 2) {
+  console.log('Argument found');
+} else {
+  console.log('No argument');
 }
