84d1e49b531f96f0e2225dadf1d66b40e74fb07e
obamwonyi <developer.oduwa@gmail.com>
2024-02-12 18:42:55 +0100

    added task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,12 +1,6 @@
 #!/usr/bin/node
-if (process.argv.length === 2) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
-} else if (process.argv.length === 3) {
-  console.log(process.argv[2]);
 } else {
-  process.argv.forEach(function (currentValue, index) {
-    if (index > 1) {
-      console.log(currentValue);
-    }
-  });
+  console.log(process.argv[2]);
 }
