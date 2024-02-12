7f5102737623affae688166b29ed1bea8bc709ab
obamwonyi <developer.oduwa@gmail.com>
2024-02-12 18:14:49 +0100

    refixed task 2 error

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 const argv = process.argv;
 
-if (argv.length > 2) {
+if (argv.length === 2) {
+  console.log('No argument');
+} else if (argv.length === 3) {
   console.log('Argument found');
 } else {
-  console.log('No argument');
+  console.log('Argument found');
 }
