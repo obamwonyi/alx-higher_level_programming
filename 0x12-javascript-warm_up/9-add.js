32e5b029ebf266e4e853e8c214f318011a8c721c
obamwonyi <developer.oduwa@gmail.com>
2024-02-14 16:21:04 +0100

    added task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
-
-const a = parseInt(process.argv[2]);
-const b = parseInt(process.argv[3]);
-
 function add (a, b) {
-  console.log(a + b);
+  const c = a + b;
+  console.log(c);
 }
 
-add(a, b);
+add(Number(process.argv[2]), Number(process.argv[3]));
