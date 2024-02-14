a3fcfceabcfd1d19b054320e710421d9b3e43910
obamwonyi <developer.oduwa@gmail.com>
2024-02-14 15:37:26 +0100

    added task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const size = process.argv[2];
+if (!(size)) {
+  console.log('Missing size');
+} else {
+  for (let x = 0; x < size; x++) {
+    let output = '';
+    for (let i = 0; i < size; i++) {
+      output = output.concat('X');
+    }
+    console.log(output);
+  }
+}
